e6bb6a356d3a29e5a19f8bde4dc94720b10eafac
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-30 10:16:14 -0700

    add migration to update database
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

---
+++ TravelApi/Migrations/20200330165002_RemoveLocations.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Migrations
+{
+    public partial class RemoveLocations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Locations_LocationId",
+                table: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_LocationId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Reviews",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Reviews",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Reviews",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(maxLength: 50, nullable: false),
+                    Country = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_LocationId",
+                table: "Reviews",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Locations_LocationId",
+                table: "Reviews",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

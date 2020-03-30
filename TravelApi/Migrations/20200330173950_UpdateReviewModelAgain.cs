788a6f47d24dedf31e691e658b6e3ce96cb57f22
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-30 10:42:31 -0700

    add destination field to review model, add migration to update database
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

---
+++ TravelApi/Migrations/20200330173950_UpdateReviewModelAgain.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Migrations
+{
+    public partial class UpdateReviewModelAgain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Destination",
+                table: "Reviews",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Destination",
+                table: "Reviews");
+        }
+    }
+}
--- TravelApi/Models/Review.cs
+++ TravelApi/Models/Review.cs
@@ -6,6 +6,9 @@ namespace TravelApi.Models
   {
     public int ReviewId { get; set; }
     [Required]
+    [StringLength(100)]
+    public string Destination { get; set; }
+    [Required]
     [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5 stars.")]
     public int Rating { get; set; }
     [Required]

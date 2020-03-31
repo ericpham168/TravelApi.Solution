b2343e29c3e41f67f108905209cbe787296071d7
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-31 10:36:50 -0700

    change token expiration date
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- TravelApi/Services/UserService.cs
+++ TravelApi/Services/UserService.cs
@@ -50,7 +50,7 @@ namespace TravelApi.Services
         {
           new Claim(ClaimTypes.Name, user.Id.ToString())
         }),
-        Expires = DateTime.UtcNow.AddDays(7),
+        Expires = DateTime.UtcNow.AddDays(1),
         SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
       };
       var token = tokenHandler.CreateToken(tokenDescriptor);

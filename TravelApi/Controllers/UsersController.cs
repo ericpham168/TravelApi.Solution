6a0a452feaf3fbf363efd7a309f3751e8c1313f7
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-31 14:43:39 -0700

    adjust indentation
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- TravelApi/Controllers/UsersController.cs
+++ TravelApi/Controllers/UsersController.cs
@@ -7,41 +7,41 @@ using System.Linq;
 namespace TravelApi.Controllers
 {
   [Authorize]
-    [ApiController]
-    [Route("api/[controller]")]
-    [ApiVersion("1.0")]
-    public class UsersController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  [ApiVersion("1.0")]
+  public class UsersController : ControllerBase
+  {
+    private IUserService _userService;
+
+    public UsersController(IUserService userService)
     {
-      private IUserService _userService;
+      _userService = userService;
+    }
 
-      public UsersController(IUserService userService)
+    // POST /users/authenticate
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody]User userParam)
+    {
+      var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+      if (user == null)
       {
-        _userService = userService;
+        return BadRequest(new { message = "Username or password is incorrect" });
       }
-
-      // POST /users/authenticate
-      [AllowAnonymous]
-      [HttpPost("authenticate")]
-      public IActionResult Authenticate([FromBody]User userParam)
+      else
       {
-        var user = _userService.Authenticate(userParam.Username, userParam.Password);
-
-        if (user == null)
-        {
-          return BadRequest(new { message = "Username or password is incorrect" });
-        }
-        else
-        {
-          return Ok(user);
-        }
-      }
-      
-      // GET /users
-      [HttpGet]
-      public IActionResult GetAll()
-      {
-        var users = _userService.GetAll();
-        return Ok(users);
+        return Ok(user);
       }
     }
+    
+    // GET /users
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      var users = _userService.GetAll();
+      return Ok(users);
+    }
+  }
 }
